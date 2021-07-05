89f7cabbf3bdab5cc3cfa53f2265ed1f7a4cd87f
Yann IRBAH <yann.irbah@protonmail.com>
2021-07-05 11:20:02 +0200

    Add homebrew packages and aliases

--- dot_config/zsh/config.d/aliases.zsh
+++ dot_config/zsh/config.d/aliases.zsh
@@ -9,6 +9,15 @@ alias ll='ls -l'
 alias la='ls -la'
 alias lt='ls --tree'
 
+# Git
+
+alias gst='git status'
+
+# Kubernetes
+
+alias k='kubectl'
+alias wk='watch kubectl'
+
 # SHELL 
 
 alias reload!='exec "$SHELL" -l'
