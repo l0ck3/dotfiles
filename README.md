8b750dc5aef5adba907b9d2647323f474d7c92be
Yann IRBAH <yann.irbah@protonmail.com>
2021-07-05 11:02:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+These are my personal dotfiles managed with `chezmoi`.
+
+## TODO
+
+- [ ] VSCodium settings
+- [ ] use `lf` as a file manager
---
+++ dot_config/zsh/config.d/aliases.zsh
@@ -0,0 +1,14 @@
+# VSCode
+
+alias code='codium'
+
+# LS 
+
+alias ls='lsd'
+alias ll='ls -l'
+alias la='ls -la'
+alias lt='ls --tree'
+
+# SHELL 
+
+alias reload!='exec "$SHELL" -l'
